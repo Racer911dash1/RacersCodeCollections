038e35c10b075e7663560e15f21051e312a34138
Racer911-1 <douwe.bakker.03@gmail.com>
2025-08-25 12:41:21 +0200

    Added subtraction

--- libs/rcc_math/include/rcc_mathcros.h
+++ libs/rcc_math/include/rcc_mathcros.h
@@ -31,4 +31,30 @@
 	long double:		__rcc_add_ldouble \
 )(a, b)
 
+/**
+ * @brief Compute the difference of @p a and @p b.
+ *
+ * No overflow checks are performed.
+ *
+ * @param[in]	a	Number to subtract.
+ * @param[in]	b	The subtrahend.
+ *
+ * @return The difference of @p a and @p b.
+ */
+# define rcc_subtract(a, b) _Generic((a), \
+	long long:			__rcc_subtract_llong, \
+	int8_t:				__rcc_subtract_int8, \
+	int16_t:			__rcc_subtract_int16, \
+	int32_t:			__rcc_subtract_int32, \
+	int64_t:			__rcc_subtract_int64, \
+	unsigned long long:	__rcc_subtract_ullong, \
+	uint8_t:			__rcc_subtract_uint8, \
+	uint16_t:			__rcc_subtract_uint16, \
+	uint32_t:			__rcc_subtract_uint32, \
+	uint64_t:			__rcc_subtract_uint64, \
+	float:				__rcc_subtract_float, \
+	double:				__rcc_subtract_double, \
+	long double:		__rcc_subtract_ldouble \
+)(a, b)
+
 #endif
--- libs/rcc_math/src/rcc_arithmetic.c
+++ libs/rcc_math/src/rcc_arithmetic.c
@@ -64,3 +64,68 @@ long double	__rcc_add_ldouble(long double a, long double b)
 {
 	return (a + b);
 }
+
+long long	__rcc_subtract_llong(long long a, long long b)
+{
+	return (a - b);
+}
+
+int8_t	__rcc_subtract_int8(int8_t a, int8_t b)
+{
+	return (a - b);
+}
+
+int16_t	__rcc_subtract_int16(int16_t a, int16_t b)
+{
+	return (a - b);
+}
+
+int32_t	__rcc_subtract_int32(int32_t a, int32_t b)
+{
+	return (a - b);
+}
+
+int64_t	__rcc_subtract_int64(int64_t a, int64_t b)
+{
+	return (a - b);
+}
+
+unsigned long long	__rcc_subtract_ullong(unsigned long long a, unsigned long long b)
+{
+	return (a - b);
+}
+
+uint8_t	__rcc_subtract_uint8(uint8_t a, uint8_t b)
+{
+	return (a - b);
+}
+
+uint16_t	__rcc_subtract_uint16(uint16_t a, uint16_t b)
+{
+	return (a - b);
+}
+
+uint32_t	__rcc_subtract_uint32(uint32_t a, uint32_t b)
+{
+	return (a - b);
+}
+
+uint64_t	__rcc_subtract_uint64(uint64_t a, uint64_t b)
+{
+	return (a - b);
+}
+
+float	__rcc_subtract_float(float a, float b)
+{
+	return (a - b);
+}
+
+double	__rcc_subtract_double(double a, double b)
+{
+	return (a - b);
+}
+
+long double	__rcc_subtract_ldouble(long double a, long double b)
+{
+	return (a - b);
+}
