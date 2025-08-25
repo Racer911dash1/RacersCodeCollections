fc7709bacb79b06695a29a9704142e9435f84a60
Racer911-1 <douwe.bakker.03@gmail.com>
2025-08-25 15:15:50 +0200

    Added multiplication

--- libs/rcc_math/include/internal/rcc_mathcros_internal.h
+++ libs/rcc_math/include/internal/rcc_mathcros_internal.h
@@ -30,4 +30,18 @@ uint64_t			__rcc_subtract_uint64(uint64_t a, uint64_t b);
 float				__rcc_subtract_float(float a, float b);
 double				__rcc_subtract_double(double a, double b);
 long double			__rcc_subtract_ldouble(long double a, long double b);
+
+long long			__rcc_multiply_llong(long long a, long long b);
+int8_t				__rcc_multiply_int8(int8_t a, int8_t b);
+int16_t				__rcc_multiply_int16(int16_t a, int16_t b);
+int32_t				__rcc_multiply_int32(int32_t a, int32_t b);
+int64_t				__rcc_multiply_int64(int64_t a, int64_t b);
+unsigned long long	__rcc_multiply_ullong(unsigned long long a, unsigned long long b);
+uint8_t				__rcc_multiply_uint8(uint8_t a, uint8_t b);
+uint16_t			__rcc_multiply_uint16(uint16_t a, uint16_t b);
+uint32_t			__rcc_multiply_uint32(uint32_t a, uint32_t b);
+uint64_t			__rcc_multiply_uint64(uint64_t a, uint64_t b);
+float				__rcc_multiply_float(float a, float b);
+double				__rcc_multiply_double(double a, double b);
+long double			__rcc_multiply_ldouble(long double a, long double b);
 #endif
--- libs/rcc_math/include/rcc_mathcros.h
+++ libs/rcc_math/include/rcc_mathcros.h
@@ -57,4 +57,30 @@
 	long double:		__rcc_subtract_ldouble \
 )(a, b)
 
+/**
+ * @brief Compute the product of @p a and @p b.
+ *
+ * No overflow checks are performed.
+ *
+ * @param[in]	a	Number to multiply.
+ * @param[in]	b	The multiplicand.
+ *
+ * @return The product of @p a and @p b.
+ */
+# define rcc_multiply(a, b) _Generic((a), \
+	long long:			__rcc_multiply_llong, \
+	int8_t:				__rcc_multiply_int8, \
+	int16_t:			__rcc_multiply_int16, \
+	int32_t:			__rcc_multiply_int32, \
+	int64_t:			__rcc_multiply_int64, \
+	unsigned long long:	__rcc_multiply_ullong, \
+	uint8_t:			__rcc_multiply_uint8, \
+	uint16_t:			__rcc_multiply_uint16, \
+	uint32_t:			__rcc_multiply_uint32, \
+	uint64_t:			__rcc_multiply_uint64, \
+	float:				__rcc_multiply_float, \
+	double:				__rcc_multiply_double, \
+	long double:		__rcc_multiply_ldouble \
+)(a, b)
+
 #endif
